9093aa9b195aa2dcf157a81fb98f03f795068d0b
Liu Zicheng <liuzc_seu@qq.com>
2025-06-18 10:20:58 +0800

    add model handler for more ablation studies

--- models/backbone.py
+++ models/backbone.py
@@ -92,3 +92,133 @@ class RegModel(nn.Module):
 
         reg_output = self.regression_head(feature)
         return reg_output
+
+
+class LSTMplusModel(nn.Module):
+    def __init__(self, input_size=20, hidden_size=64, output_feature=2, num_layer=2):
+        super(LSTMplusModel, self).__init__()
+        self.bn = nn.BatchNorm1d(input_size)
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layer, dropout=0.3)
+        self.fc = nn.Linear(hidden_size, output_feature)
+        self.classify_head = nn.Linear(hidden_size, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1)
+        x_normalized = self.bn(x)
+        x_normalized = x_normalized.permute(0, 2, 1)
+
+        lstm_out, _ = self.lstm(x_normalized)
+        reg_output = self.fc(lstm_out[:, -1, :])
+        class_output = self.sigmoid(self.classify_head(torch.mean(lstm_out, dim=1)))
+        return class_output, reg_output
+
+
+class MTLTransModel(nn.Module):
+    def __init__(self, input_size=20, hidden_size=64, lstm_hidden_size=64, output_feature=32, num_layer=2, nhead=2):
+        super(MTLTransModel, self).__init__()
+        self.output_feature = output_feature
+        self.hidden_dim = hidden_size
+        self.lstm_hidden_size = lstm_hidden_size
+
+        self.bn = nn.BatchNorm1d(input_size)
+        self.input_fc = nn.Linear(input_size, hidden_size)
+
+        self.transformer_encoder_layer = CustomizedTransformerEncoderLayer(d_model=lstm_hidden_size, nhead=nhead,
+                                                                           dropout=0.3)
+        self.transformer_encoder = CustomizedTransformerEncoder(self.transformer_encoder_layer,
+                                                                num_layers=num_layer * 2)
+
+        self.fc = nn.Linear(lstm_hidden_size, output_feature)
+
+        self.classify_head = nn.Linear(output_feature, 1)
+        self.regression_head = nn.Linear(output_feature, 2)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1)
+        x_normalized = self.bn(x)
+        x_normalized = x_normalized.permute(2, 0, 1)
+        x_embedded = self.input_fc(x_normalized)
+
+        transformer_out = self.transformer_encoder(x_embedded)
+        feature = self.fc(transformer_out[-1, :, :]).view(-1, self.output_feature)
+
+        class_output = self.sigmoid(self.classify_head(feature))
+        reg_output = self.regression_head(feature)
+        return class_output, reg_output
+
+
+class MTLLstmModel(nn.Module):
+    def __init__(self, input_size=20, hidden_size=64, lstm_hidden_size=64, output_feature=32, num_layer=2, nhead=2):
+        super(MTLLstmModel, self).__init__()
+        self.output_feature = output_feature
+        self.hidden_dim = hidden_size
+        self.lstm_hidden_size = lstm_hidden_size
+
+        self.bn = nn.BatchNorm1d(input_size)
+        self.input_fc = nn.Linear(input_size, hidden_size)
+
+        self.bi_lstm = nn.LSTM(input_size=hidden_size, hidden_size=lstm_hidden_size, num_layers=num_layer * 2,
+                               bidirectional=True, batch_first=False)
+
+        self.fc = nn.Linear(lstm_hidden_size * 2, output_feature)
+
+        self.classify_head = nn.Linear(output_feature, 1)
+        self.regression_head = nn.Linear(output_feature, 2)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1)
+        x_normalized = self.bn(x)
+        x_normalized = x_normalized.permute(2, 0, 1)
+        x_embedded = self.input_fc(x_normalized)
+
+        lstm_out, _ = self.bi_lstm(x_embedded)
+        feature = self.fc(lstm_out[-1, :, :])
+
+        class_output = self.sigmoid(self.classify_head(feature))
+        reg_output = self.regression_head(feature)
+        return class_output, reg_output
+
+
+class MTLRevModel(nn.Module):
+    def __init__(self, input_size=20, hidden_size=64, lstm_hidden_size=64, output_feature=32, num_layer=2, nhead=2):
+        super(MTLRevModel, self).__init__()
+        self.output_feature = output_feature
+        self.hidden_dim = hidden_size
+        self.lstm_hidden_size = lstm_hidden_size
+
+        self.bn = nn.BatchNorm1d(input_size)
+        self.input_fc = nn.Linear(input_size, hidden_size)
+
+        self.transformer_encoder_layer = CustomizedTransformerEncoderLayer(d_model=lstm_hidden_size, nhead=nhead,
+                                                                           dropout=0.3)
+        self.transformer_encoder = CustomizedTransformerEncoder(self.transformer_encoder_layer,
+                                                                num_layers=num_layer)
+        self.bi_lstm = nn.LSTM(input_size=hidden_size, hidden_size=lstm_hidden_size, num_layers=num_layer,
+                               bidirectional=True, batch_first=False)
+
+        self.fc = nn.Linear(lstm_hidden_size * 2, output_feature)
+
+        self.classify_head = nn.Linear(output_feature, 1)
+        self.regression_head = nn.Linear(output_feature, 2)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1)
+        x_normalized = self.bn(x)
+        x_normalized = x_normalized.permute(2, 0, 1)
+        x_embedded = self.input_fc(x_normalized)
+
+        transformer_out = self.transformer_encoder(x_embedded)
+        print("train_out".format(transformer_out.shape))
+        lstm_out, _ = self.bi_lstm(transformer_out)
+        print("lstm_out".format(lstm_out.shape))
+        feature = self.fc(lstm_out[-1, :, :]).view(-1, self.output_feature)
+        print("feature".format(feature.shape))
+
+        class_output = self.sigmoid(self.classify_head(feature))
+        reg_output = self.regression_head(feature)
+        return class_output, reg_output
+
--- scripts/modelhandler.py
+++ scripts/modelhandler.py
@@ -89,3 +89,87 @@ class RegHandler(ModelHandler):
         logging.info(
             "--------------Fold {} Evaluating: mse: {:.4f} mis: {:.4f}--------------"
             .format(fold + 1, mse, mis))
+
+
+class LSTMplusHandler(ModelHandler):
+    def __init__(self, args):
+        super().__init__(args)
+
+    def create_model(self):
+        return LSTMplusModel().to(self.args.device)
+
+    def train(self, model, train_loader, val_loader, optimizer, scheduler, early_stop):
+        train_MTL(self.args, model=model, train_loader=train_loader, val_loader=val_loader,
+                  optim=optimizer, scheduler=scheduler, earlystop=early_stop)
+
+    def test(self, model, test_loader):
+        return test_MTL(self.args, model, test_loader)
+
+    def log_results(self, metrics, fold):
+        acc, f1, mse, mis = metrics
+        logging.info(
+            "--------------Fold {} Evaluating: acc: {:.4f}% f1: {:.4f} mse: {:.4f} mis: {:.4f}--------------"
+            .format(fold + 1, acc * 100, f1, mse, mis))
+
+
+class MTLTransHandler(ModelHandler):
+    def __init__(self, args):
+        super().__init__(args)
+
+    def create_model(self):
+        return MTLTransModel().to(self.args.device)
+
+    def train(self, model, train_loader, val_loader, optimizer, scheduler, early_stop):
+        train_MTL(self.args, model=model, train_loader=train_loader, val_loader=val_loader,
+                  optim=optimizer, scheduler=scheduler, earlystop=early_stop)
+
+    def test(self, model, test_loader):
+        return test_MTL(self.args, model, test_loader)
+
+    def log_results(self, metrics, fold):
+        acc, f1, mse, mis = metrics
+        logging.info(
+            "--------------Fold {} Evaluating: acc: {:.4f}% f1: {:.4f} mse: {:.4f} mis: {:.4f}--------------"
+            .format(fold + 1, acc * 100, f1, mse, mis))
+
+
+class MTLLstmHandler(ModelHandler):
+    def __init__(self, args):
+        super().__init__(args)
+
+    def create_model(self):
+        return MTLLstmModel().to(self.args.device)
+
+    def train(self, model, train_loader, val_loader, optimizer, scheduler, early_stop):
+        train_MTL(self.args, model=model, train_loader=train_loader, val_loader=val_loader,
+                  optim=optimizer, scheduler=scheduler, earlystop=early_stop)
+
+    def test(self, model, test_loader):
+        return test_MTL(self.args, model, test_loader)
+
+    def log_results(self, metrics, fold):
+        acc, f1, mse, mis = metrics
+        logging.info(
+            "--------------Fold {} Evaluating: acc: {:.4f}% f1: {:.4f} mse: {:.4f} mis: {:.4f}--------------"
+            .format(fold + 1, acc * 100, f1, mse, mis))
+
+
+class MTLRevHandler(ModelHandler):
+    def __init__(self, args):
+        super().__init__(args)
+
+    def create_model(self):
+        return MTLRevModel().to(self.args.device)
+
+    def train(self, model, train_loader, val_loader, optimizer, scheduler, early_stop):
+        train_MTL(self.args, model=model, train_loader=train_loader, val_loader=val_loader,
+                  optim=optimizer, scheduler=scheduler, earlystop=early_stop)
+
+    def test(self, model, test_loader):
+        return test_MTL(self.args, model, test_loader)
+
+    def log_results(self, metrics, fold):
+        acc, f1, mse, mis = metrics
+        logging.info(
+            "--------------Fold {} Evaluating: acc: {:.4f}% f1: {:.4f} mse: {:.4f} mis: {:.4f}--------------"
+            .format(fold + 1, acc * 100, f1, mse, mis))
